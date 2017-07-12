14e9baebbd0f996fb0cd34569336c158f7c4473a
tim <tim.krusch@web.de>
2017-07-12 17:29:59 +0200

    added button for filebrowser to hide

--- src/scenes/testscene/components/testcomp.js
+++ src/scenes/testscene/components/testcomp.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
-  Container,
+  Sidebar,
   Icon,
-  Message,
+  Segment,
   Menu,
   Dropdown,
   Button,
-  Breadcrumb
+  Breadcrumb,
+  Header,
+  state,
+  toggleVisibility,
+  List
 } from 'semantic-ui-react'
 
 const branches = [
@@ -14,43 +18,148 @@ const branches = [
   { key: 'branch_test1', text: 'branch_test1', value: 'branch_test1' },
   { key: 'branch_test2', text: 'branch_test2', value: 'branch_test2' }
 ]
+class testmain extends Component {
+  state = { visible: true }
 
-const testmain = () =>
-  <Menu borderless>
-    <Menu.Item content="Branches:" />
-    <Dropdown basic options={branches} defaultValue="master" simple item />
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
 
-    <Menu.Item disabled />
+  render() {
+    const { visible } = this.state
+    return (
+      <div>
+        {' '}<Menu borderless>
+          <Button.Group>
+            <Button
+              basic
+              onClick={this.toggleVisibility}
+              content="Filebrowser"
+              icon="sitemap"
+            />
+          </Button.Group>
+          <Menu.Item content="Branches:" />
+          <Dropdown
+            basic
+            options={branches}
+            defaultValue="master"
+            simple
+            item
+          />
 
-    <Menu.Item>
-      <Breadcrumb>
-        <Breadcrumb.Section link>user</Breadcrumb.Section>
-        <Breadcrumb.Divider />
-        <Breadcrumb.Section acitve>Repo1</Breadcrumb.Section>
-      </Breadcrumb>
-    </Menu.Item>
+          <Menu.Item disabled />
 
-    <Menu.Menu position="right">
-      <Button.Group floated="right">
-        <Button basic>
-          <Icon.Group>
-            <Icon name="file" />
-            <Icon corner name="plus" />
-          </Icon.Group>
-          {' create File'}
-        </Button>
-        <Button basic>
-          <Icon.Group>
-            <Icon name="folder" />
-            <Icon corner name="plus" />
-          </Icon.Group>
-          {' create Folder'}
-        </Button>
-        <Button basic content="upload File" icon="upload" />
-        <Button basic content="download" icon="download" />
-      </Button.Group>
-    </Menu.Menu>
-  </Menu>
+          <Menu.Item>
+            <Breadcrumb>
+              <Breadcrumb.Section link>user</Breadcrumb.Section>
+              <Breadcrumb.Divider />
+              <Breadcrumb.Section acitve>Repo1</Breadcrumb.Section>
+            </Breadcrumb>
+          </Menu.Item>
+
+          <Menu.Menu position="right">
+            <Button.Group floated="right">
+              <Button basic>
+                <Icon.Group>
+                  <Icon name="file" />
+                  <Icon corner name="plus" />
+                </Icon.Group>
+                {' create File'}
+              </Button>
+              <Button basic>
+                <Icon.Group>
+                  <Icon name="folder" />
+                  <Icon corner name="plus" />
+                </Icon.Group>
+                {' create Folder'}
+              </Button>
+              <Button basic content="upload File" icon="upload" />
+              <Button basic content="download" icon="download" />
+            </Button.Group>
+          </Menu.Menu>
+        </Menu>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={List}
+            animation="slide out"
+            width="wide"
+            visible={visible}
+            icon="labeled"
+            vertical
+          >
+            <List.Item>
+              <List.Icon name="folder" />
+              <List.Content>
+                <List.Header>src</List.Header>
+                <List.Description>Source files for project</List.Description>
+                <List.List>
+                  <List.Item>
+                    <List.Icon name="folder" />
+                    <List.Content>
+                      <List.Header>site</List.Header>
+                      <List.Description>Your site's theme</List.Description>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon name="folder" />
+                    <List.Content>
+                      <List.Header>themes</List.Header>
+                      <List.Description>Packaged theme files</List.Description>
+                      <List.List>
+                        <List.Item>
+                          <List.Icon name="folder" />
+                          <List.Content>
+                            <List.Header>default</List.Header>
+                            <List.Description>
+                              Default packaged theme
+                            </List.Description>
+                          </List.Content>
+                        </List.Item>
+                        <List.Item>
+                          <List.Icon name="folder" />
+                          <List.Content>
+                            <List.Header>my_theme</List.Header>
+                            <List.Description>
+                              Packaged themes are also available in this folder
+                            </List.Description>
+                          </List.Content>
+                        </List.Item>
+                      </List.List>
+                    </List.Content>
+                  </List.Item>
+                  <List.Item>
+                    <List.Icon name="file" />
+                    <List.Content>
+                      <List.Header>theme.config</List.Header>
+                      <List.Description>
+                        Config file for setting packaged themes
+                      </List.Description>
+                    </List.Content>
+                  </List.Item>
+                </List.List>
+              </List.Content>
+            </List.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Segment>
+              <Header as="h3">Application Content</Header>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+}
 
 export { testmain }
 
